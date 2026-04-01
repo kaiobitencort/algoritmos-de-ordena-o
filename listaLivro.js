e312a71a56c2c2fd7cb967ad2db1925203050e4b
kaiobitencort <kaio.bitencort@escola.pr.gov.br>
2026-04-01 11:42:32 +0000

    trabalho finalizado

---
+++ listaLivro.js
@@ -0,0 +1,27 @@
+const precosLivros = [
+    {
+        titulo: "Tazercraft",
+        preco: 125.50
+    }, 
+    {
+        titulo: "O hobbit",
+        preco: 200
+    }, 
+    {
+        titulo: "Edukof",
+        preco: 75
+    }, 
+    {
+        titulo: "Authenticgames",
+        preco:50
+    }, 
+    {
+        titulo: "Drible do ney",
+        preco: 100
+    }, 
+    {
+        titulo: "albun da copa do mundo 2026",
+        preco: 80
+    }];
+
+ module.exports = precosLivros;
